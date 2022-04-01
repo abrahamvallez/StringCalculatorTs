d482e533571846433292d5c0a0151c4498bb49ed
Abraham Vallez <abevallez@gmail.com>
2022-04-02 00:54:29 +0200

    refactor: extract to methods in order to clean code

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,22 +1,36 @@
 export const Add = (input: string): number => {
-    let separatorRegex: RegExp = /,|\n/
-    let numbers:string[] = []
-    
+    let negatives:string[] = []
+    let numbers = getNumberFromInput(input)
 
-    if (input === '') {
-        return 0
-    } else if (input.length === 1) {
-        return parseInt(input)
-    } else if(input.startsWith('//')) {
-        separatorRegex = new RegExp(input.substring(2, input.indexOf('\n')))
-        input = input.substring(input.indexOf('\n') + 1)
-    }
-
-    numbers = input.split(separatorRegex)
-
-    let negatives: string[] = numbers.filter(n => parseInt(n) < 0)
-    if(negatives.length > 0) {
+    negatives = numbers.filter(n => parseInt(n) < 0)
+    if((numbers.filter(n => parseInt(n) < 0)).length > 0) {
         throw new Error('negatives not allowed: ' + negatives.join(','))
     }
+
     return numbers.reduce((sum, currentNumber) => sum + parseInt(currentNumber), 0)
+}
+
+const getNumberFromInput = (input: string): string[] => {
+    let numbers: string[] = []
+
+    if(input === '') {
+        numbers = ['0']
+    } else if (input.length === 1) {
+        numbers = [input]
+    } else if(input.startsWith('//')) {
+        numbers = getNumbersWithCustomSeparator(input)
+    } else {
+        numbers = getNumbersWithDefaultSeparator(input)
+    }
+
+    return numbers
+}
+
+const getNumbersWithCustomSeparator = (input: string): string[] => {
+    const separatorRegex: RegExp = new RegExp(input.substring(2, input.indexOf('\n')))
+    return input.substring(input.indexOf('\n') + 1).split(separatorRegex)
+}
+
+const getNumbersWithDefaultSeparator = (input: string): string[] => {
+    return input.split(/,|\n/)
 }
