e4ad63bd943409fd513534c279a57293158278bd
Abraham Vallez <abevallez@gmail.com>
2022-04-01 23:19:58 +0200

    feat: result should be the sum when inputs are two numbers -red

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -8,4 +8,8 @@ describe('String Calculator', () => {
     it.each([['1', 1], ['2', 2], ['3', 3]])('result should be the same number when input is only one number', (input: string, expectedResult) => {
         expect(Add(input)).toBe(expectedResult)
     })
+
+    it('result should be the sum when inputs are two numbers', () => {
+        expect(Add('1,2')).toBe(3)
+    })
 })
