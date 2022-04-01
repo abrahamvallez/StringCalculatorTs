ad265b473fdef94fe2139f5e57e49c9e11c7eb82
Abraham Vallez <abevallez@gmail.com>
2022-04-01 23:05:01 +0200

    feat: result should be 1  when input is 1 - red

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -1,8 +1,11 @@
 import {Add} from './StringCalculator';
 
 describe('String Calculator', () => {
-    it('should be result 0 when empty entry', () => {
-        const result = Add('')
-        expect(result).toBe(0)
-    });
+    it('result should be 0 when empty entry', () => {
+        expect(Add('')).toBe(0)
+    })
+
+    it('result should be 1 when input is 1', () => {
+        expect(Add('1')).toBe(1)
+    })
 })
