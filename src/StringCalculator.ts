79d282ba3ca77c0d33a20dab9a0ce25820d61c80
Abraham Vallez <abevallez@gmail.com>
2022-04-02 00:17:11 +0200

    feat: first test allowing specifying custom separators - green

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,10 +1,17 @@
 export const Add = (input: string): number => {
+    let separatorRegex: RegExp = /,|\n/
+    let numbers:string[] = []
+    
     if (input === '') {
         return 0
     } else if (input.length === 1) {
         return parseInt(input)
+    } else if(input.startsWith('//')) {
+        separatorRegex = new RegExp(input.substring(2, input.indexOf('\n')))
+        numbers = input.substring(input.indexOf('\n') + 1).split(separatorRegex)
+        return numbers.reduce((sum, currentNumber) => sum + parseInt(currentNumber), 0)
     }
 
-    const numbers = input.split(/,|\n/)
+    numbers = input.split(separatorRegex)
     return numbers.reduce((sum, currentNumber) => sum + parseInt(currentNumber), 0)
 }
