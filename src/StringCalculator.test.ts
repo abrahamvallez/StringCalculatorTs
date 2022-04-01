7cc1307a8aaf4874dce1021c70980b8fb17c4e7c
Abraham Vallez <abevallez@gmail.com>
2022-04-01 23:07:00 +0200

    feat: result should be 2  when input is 2 - red

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -8,4 +8,8 @@ describe('String Calculator', () => {
     it('result should be 1 when input is 1', () => {
         expect(Add('1')).toBe(1)
     })
+
+    it('result should be 2 when input is 2', () => {
+        expect(Add('2')).toBe(2)
+    })
 })
