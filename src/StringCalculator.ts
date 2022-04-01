4e087883222aac53e658f772fa29e696448b8098
Abraham Vallez <abevallez@gmail.com>
2022-04-01 23:08:58 +0200

    feat: result should be 3 when input is 3 - green

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,6 +3,8 @@ export const Add = (input: string) => {
         return 1 
     } else if (input == '2') { 
         return 2 
+    } else if (input == '3') {
+        return 3 
     }
 
     return 0
